555af9e09875eead70c501103ba6395994048592
Michel Perez <michel.ingesoft@gmail.com>
2015-02-19 16:57:11 -0500

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true
-	has_many :sells
+  validates :name, presence: true
+  has_many :sells
+
+  after_create :send_email
 
   def login
     User.where(email: self.email, password: self.password).first
+  end
+
+  #callback for sending the email
+  def send_email
+    # RegisterMailer.registration(self).deliver_now
+    SendEmailJob.set(wait: 20.seconds).perform_later(self)
   end
 
 end
